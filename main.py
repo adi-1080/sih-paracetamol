996ed6235de78cc1de3275a614c4826810cbf85e
adi-1080 <adityagupta5277@gmail.com>
2025-09-25 02:00:51 +0530

    gcp

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app.include_router(mobile_model.router)
 
 def main():
     print("Hello from ml-backend!")
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)  
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)  
 
 if __name__ == "__main__":
     main()
